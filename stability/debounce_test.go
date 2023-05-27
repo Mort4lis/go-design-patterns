47d133ae31e52a59f5b8d6d3060d9f7ce8e07fc1
Pavel Korchagin <mortalis94@gmail.com>
2023-05-27 17:39:01 +0300

    [test] extend tests for Debounce

--- stability/debounce_test.go
+++ stability/debounce_test.go
@@ -21,6 +21,26 @@ func counter() Circuit {
 	}
 }
 
+func TestDebounceFirst(t *testing.T) {
+	ctx := context.Background()
+	debounce := DebounceFirst(counter(), time.Second)
+
+	res, _ := debounce(ctx)
+	if res != "1" {
+		t.Errorf("wrong debounce result: got %s, want 1", res)
+	}
+
+	time.Sleep(900 * time.Millisecond)
+
+	for i := 0; i < 10; i++ {
+		res, _ = debounce(ctx)
+	}
+
+	if res != "1" {
+		t.Errorf("wrong debounce result: got %s, want 1", res)
+	}
+}
+
 // TestDebounceFirstDataRace tests for data races.
 func TestDebounceFirstDataRace(t *testing.T) {
 	ctx := context.Background()
@@ -61,6 +81,28 @@ func TestDebounceFirstDataRace(t *testing.T) {
 	}
 
 	wg.Wait()
+}
+
+func TestDebounceLast(t *testing.T) {
+	var res string
+
+	ctx := context.Background()
+	debounce := DebounceLast(counter(), time.Second)
+
+	for i := 0; i < 10; i++ {
+		res, _ = debounce(ctx)
+	}
+
+	if res != "" {
+		t.Errorf("wrong debounce result: got %s, want empty string", res)
+	}
+
+	time.Sleep(1100 * time.Millisecond)
+
+	res, _ = debounce(ctx)
+	if res != "1" {
+		t.Errorf("wrong debounce result: got %s, want 1", res)
+	}
 }
 
 // TestDebounceLastDataRace tests for data races.
