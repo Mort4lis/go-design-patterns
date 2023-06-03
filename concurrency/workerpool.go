91e5d13ed658aa063d3e2c019b99b958505801b9
Pavel Korchagin <mortalis94@gmail.com>
2023-06-03 19:21:35 +0300

    [doc] add documentation for concurrency.WorkerPool

--- concurrency/workerpool.go
+++ concurrency/workerpool.go
@@ -4,8 +4,14 @@ import (
 	"sync"
 )
 
+// WorkerFunc is a function type that workers perform.
 type WorkerFunc func(in any) any
 
+// WorkerPool pattern distributes the work across multiple workers (goroutines) concurrently.
+// It takes the number of workers and function of WorkerFunc type that the workers will perform.
+// It also returns two channels: the first to send some value to start processing among the workers
+// and the second to handle results. If the sending channel is closed, it'll close all the workers and
+// WorkerPool will be finished.
 func WorkerPool(n int, fn WorkerFunc) (chan<- any, <-chan any) {
 	in := make(chan any)
 	out := make(chan any)
